4f2163a9f29e191403d65c3531eaa501d94e3db4
Jeff Borland <borland2000@hotmail.com>
2015-12-23 07:21:57 -0500

    removed

--- src/MyHoot/getNameUsers.php
+++ src/MyHoot/getNameUsers.php
@@ -7,7 +7,7 @@ require 'dbsettings.php';
 $sql = "SELECT *  FROM `users` WHERE game_id ='".$_SESSION["game_id"]."'";
 $result = $conn->query($sql);
 if ($result)
-{
+{ 
    while($row = $result->fetch_assoc())
        echo $row['name'].",";
 }
