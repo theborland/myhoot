93b3fb9eb89581b879e257313ae5f9e5f2a5618e
Jeff Borland <jeffborland@ppsd.local>
2016-02-09 11:29:28 -0500

    d

--- src/MyHoot/getQuestion.php
+++ src/MyHoot/getQuestion.php
@@ -50,7 +50,7 @@ if (isset($_GET["type"]))
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="//cdn.jsdelivr.net/jquery.color-animation/1/mainfile"></script>
 <script>
-var counter = 300;
+var counter = 30;
 $(document).ready(function(){
 $('#qTimer').animate({
 	left: "+=50%",
