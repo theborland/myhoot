1a804889585ad1db593e69698deb668ee934b22e
Orkhan Nadirli <Orkhan@Orkhans-MacBook-Pro.local>
2016-03-16 15:13:50 -0400

    music

---
+++ src/MyHoot/scripts/music.js
@@ -0,0 +1,24 @@
+function mute(){
+	if(playing==true)
+		muteOff();
+	else
+		muteOn();
+}
+
+function muteOn(){
+	var music = document.getElementById("bgMusic");
+	var button = document.getElementById("muteButton");
+	music.volume = 1;
+	playing = true;
+	button.style.backgroundImage = "url(img/mute1.png)";
+	document.cookie="playMusic=true";
+}
+
+function muteOff(){
+	var music = document.getElementById("bgMusic");
+	var button = document.getElementById("muteButton");
+	music.volume = 0;
+	playing = false;
+	button.style.backgroundImage = "url(img/mute2.png)";
+    document.cookie="playMusic=false";
+}
