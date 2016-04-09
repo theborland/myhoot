b177db78ff61962500d9d54d30de64bef6abe9a7
Orkhan Nadirli <Orkhan@Orkhans-MacBook-Pro.local>
2016-04-09 13:54:18 -0400

    clouds

--- src/MyHoot/scripts/cloud.js
+++ src/MyHoot/scripts/cloud.js
@@ -5,11 +5,15 @@ class Cloud{
         this.numBoxes =     Math.floor( Math.random() * 3 ) + 1;
         this.overallTop =   Math.floor( Math.random() * docHeight * 0.6);
         this.overallLeft = -300;
-        document.getElementById("clouds").innerHTML = document.getElementById("clouds").innerHTML + "<div class='cloudWrap' id='cloudWrap_" + this.id + "'>";
+        document.getElementById("clouds").innerHTML = document.getElementById("clouds").innerHTML + "<div class='cloudWrap' id='cloudWrap_" + this.id + "'></div>";
         for(var i=0; i<this.numBoxes; i++){
-            document.getElementById("clouds").innerHTML = document.getElementById("clouds").innerHTML + "<div class='cloud' id='cloud_" + this.id + "_" + i + "''></div>";
+            document.getElementById("cloudWrap_" + this.id).innerHTML = document.getElementById("cloudWrap_" + this.id).innerHTML + "<div class='cloud' id='cloud_" + this.id + "_" + i + "''></div>";
+            document.getElementById("cloud_" + this.id + "_" + i ).style.top    = Math.floor( Math.random() * 50 + this.overallTop )         + "px";
+            document.getElementById("cloud_" + this.id + "_" + i ).style.left   = Math.floor( Math.random() * -250 + i*10 + this.overallLeft)       + "px";
+            document.getElementById("cloud_" + this.id + "_" + i ).style.width  = Math.floor( Math.random() * 300 + 100)                     + "px";
+            document.getElementById("cloud_" + this.id + "_" + i ).style.height = Math.floor( Math.random() * 50  + 20)                     + "px";
+
         }
-        document.getElementById("clouds").innerHTML = document.getElementById("clouds").innerHTML + "</div>";
     }
 
     animate(x){
