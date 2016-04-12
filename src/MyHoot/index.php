a8f344778374ccdc9f590bbd3df6b9535768bfd7
borland2000@hotmail.com <borland2000@hotmail.com>
2016-04-12 14:17:13 -0400

    jeff

---
+++ src/MyHoot/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <script type="text/javascript">
+  <!--
+  if (screen.width <= 800) {
+    window.location = "joinQuiz.php";
+  }
+  else
+    window.location = "startQuiz.php";
+  //-->
+</script>
+</head>
+<body>
+</body>
+</html>
