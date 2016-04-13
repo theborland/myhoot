7c0a9589003ef422b6918fc58cfe6b628a23c7c9
borland2000@hotmail.com <borland2000@hotmail.com>
2016-04-13 13:36:14 -0400

    jeff

--- src/MyHoot/aboutUs.php
+++ src/MyHoot/aboutUs.php
@@ -1,5 +1,22 @@
+
 Welcome to GameOn.World!  This is the first draft (beta 1.0) of our website which we hope will be a fun, communal way
 to learn geography and other interesting trivia.  The site was started by Jeff Borland, a teacher in Portland, Maine and one of his former students
 Orkhan Nadirli.  We believed that students need to know much more about the world and we thought that this game format would be a wonderful way to do that.  In the coming
 months we hope to add lots of new features (like the ability to create your own trivia sets), so keep checking back.  And if you have ideas or comments we would love to hear them.
 Enjoy playing.
+
+Who?
+Really anyone.  When we thought of GameOn.World, we imagined teachers (especially Social Studies teachers) using it for practice geography.  But we hope you find it fun enough to use anytime you want.  Maybe the next get together you have you try it out - the more people the merrier.
+
+Why?
+Well its fun but hopefully it will teach you a bit about the world too.
+
+Where?
+It works great in a classroom with a projector (the person starting the quiz will use the projector) while everyone else joins on their mobile device.  But you can play GameOn.World anywhere you have an internet connect screen (so at a party on a TV?)
+
+How?
+The person at the display will start a game.  They have an option of choosing categories and regions of the world.  They also can select autoplay (so the game just goes without user input) and also the number of rounds.
+The users on their mobile devices will join the game using the id and their choosen name.
+
+What?
+A trivia game that encourages users to learn about the world.
