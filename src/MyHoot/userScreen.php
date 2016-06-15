f12d6d6c7a0922bf347483dff517993fdaf3d964
borland2000@hotmail.com <borland2000@hotmail.com>
2016-06-15 10:38:43 -0400

    jeff

--- src/MyHoot/userScreen.php
+++ src/MyHoot/userScreen.php
@@ -43,7 +43,7 @@ function initialize() {
       mapTypeId: google.maps.MapTypeId.TERRAIN,
       stylers: [{ visibility: 'off' }]
     },
-      { featureType: "administrative.province", stylers: [ { visibility: "on" } ] },
+      { featureType: "administrative.province", stylers: [ { visibility: "off" } ] },
     { featureType: "road", stylers: [ { visibility: "off" } ] },
     {
       featureType: 'water',
