11c73b13c758c19a0ea63ee9dd97be0ac13ab2d0
borland2000@hotmail.com <borland2000@hotmail.com>
2016-09-21 07:26:23 -0400

    jeff

--- src/MyHoot/showAnswer.php
+++ src/MyHoot/showAnswer.php
@@ -37,7 +37,7 @@ $theQuestion=Question::loadQuestion();
 			//automatically forward if automode is on
 			setTimeout( function(){
 			      window.location.href='getQuestion.php';
-			}  , 9000 );
+			}  , 11000 );
 			<?php
 			}
 			?>
