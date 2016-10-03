f0d5399853c6f37f4ec198e571b1f834e21afdf3
Orkhan Nadirli <Orkhan@Orkhans-MacBook-Pro.local>
2016-10-02 22:59:06 -0400

    fb

--- src/MyHoot/waitingScreenEnd.php
+++ src/MyHoot/waitingScreenEnd.php
@@ -23,10 +23,30 @@ require 'controller/dbsettings.php';
      window.onload = function(){
          loadWaitingForNextGame('<?php echo $pusherIP; ?>' ,'<?php echo $_SESSION["game_id"]; ?>');
      }
+
+
    </script>
 
 </head>
 <body>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '203380266741886',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
 
 <div id="headerContainer">
 				<a href="#" id="logoLink"><img src="img/logo.svg" id="logo"></a>
@@ -36,12 +56,29 @@ require 'controller/dbsettings.php';
 </div>
 <div id="messageWrap">
 
-
+<input type="button" value="Share on Facebook!" id="shareOnFB">
+<br>
+<div id="surveyWrap">
+	
+</div>
 
 </div>
 <div id="tryContainer">
 	Or maybe not:
-	<a href="joinQuiz.php" class="regButton" id="tryHere">Play again!</a>
+	<a href="joinQuiz.php" class="regButton" id="tryHere" class="regButton">Play again!</a>
 </div>
+
+<script>
+	document.getElementById('shareBtn').onclick = function() {
+	  FB.ui({
+	    method: 'share',
+    	mobile_iframe: true,
+	    display: 'popup',
+	    quote: 'I just got nth place playing against my friends on GameOn.World! Start a game now, it\'s free and easy play.',
+	    href: 'https://gameon.world/game.php',
+	  }, function(response){});
+	}
+
+</script>
 </body>
 </html>
