d294004b1e59eb98dc7ab2a598c1addbdeade23d
Orkhan Nadirli <orkhanahkro@gmail.com>
2017-01-26 23:37:12 -0500

    showanswer single

--- src/MyHoot/single/showAnswer.php
+++ src/MyHoot/single/showAnswer.php
@@ -76,24 +76,29 @@ if ($submit=="Join"){
 </style>
 	<script>
 
-  var timeleft = <?php echo $timeLeft; ?> * 1000;
-  console.log(timeleft);
-  $('#timerBar').animate({
-    width: "0%"
-  }, timeleft, "linear");
 
 
-  var counter=setInterval(timer, <?php echo $timeLeft; ?> * 100000); //1000 will  run it every 1 second
-
-  function timer()
-  {
-       window.location.href = "userScreen.php";
-       clearInterval(counter);
-       count=0999;
-       return;
-  }
+    window.onload = function(){
 
 
+      var timeleft = <?php echo $timeLeft; ?> * 1000;
+      console.log(timeleft);
+      $('#timerBar').animate({
+        width: "0%"
+      }, timeleft, "linear");
+
+
+      var counter=setInterval(timer, <?php echo $timeLeft; ?> * 1000); //1000 will  run it every 1 second
+
+      function timer()
+      {
+           window.location.href = "userScreen.php";
+           clearInterval(counter);
+           count=0999;
+           return;
+      }
+
+    }
 	</script>
 </head>
 <body>
