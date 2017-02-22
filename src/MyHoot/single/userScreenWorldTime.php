96d4aa5f46c1b6853b89a121ed46e42a39d09eaa
Orkhan Nadirli <orkhan.nadirli@maine.edu>
2017-02-22 10:24:28 -0500

    usDecimal

--- src/MyHoot/single/userScreenWorldTime.php
+++ src/MyHoot/single/userScreenWorldTime.php
@@ -5,20 +5,20 @@ require '../controller/dbsettings.php';
 if (isset($_GET["question"]))
   if (Answer::checkUserSubmitted($_GET["question"],$_SESSION["user_id"]))
     header("Location: waitingScreen.php?message=".urlencode("Come on - you can't submit twice..."));
-    $_SESSION["questionNumber"]=Game::questionStatusRedirect();
-    $theQuestion=Question::loadQuestion();
+  $_SESSION["questionNumber"]=Game::questionStatusRedirect();
+  $theQuestion=Question::loadQuestion();
 
-    $seconds=time();
-    $timeLeft=($seconds%($lengthOfGame+$lengthOfBreak)-$lengthOfGame)*-1;
+  $seconds=time();
+  $timeLeft=($seconds%($lengthOfGame+$lengthOfBreak)-$lengthOfGame)*-1;
 
-?>
-<html>
+  ?>
+  <html>
   <head>
-  <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-  <link rel="stylesheet" href="../style/global.css">
-  <link rel="stylesheet" href="../style/getQuestion.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-      <link href="../style/nouislider.min.css" rel="stylesheet">
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="../style/global.css">
+    <link rel="stylesheet" href="../style/getQuestion.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <link href="../style/nouislider.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../style/inputSlider.css">
     <script src="../scripts/socketScripts.js"></script>
     <style>
@@ -53,7 +53,7 @@ if (isset($_GET["question"]))
         margin-top:-8px;
       }
 
-        #timerBar{
+      #timerBar{
         position: fixed;
         left: 0px;
         top: 0px;
@@ -96,58 +96,58 @@ if (isset($_GET["question"]))
       }
             //Math.round(value/100)*100
         //x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-    }
+      }
 
-    var counter = 30;
-    window.onload = function(){
-      $('#timer').animate({
-        width: "0%"
-      }, 30000, "linear");
+      var counter = 30;
+      window.onload = function(){
+        $('#timer').animate({
+          width: "0%"
+        }, 30000, "linear");
 
-      var interval = setInterval(function() {
+        var interval = setInterval(function() {
           counter--;
-        $('#timeLeft').html(counter);
+          $('#timeLeft').html(counter);
           if (counter == 0) {
             window.location.replace("showAnswer.php");
           }
-      }, 1000);
+        }, 1000);
 
       //animation clock
       var x=0;
       var interval = setInterval(function() {
 
-           x++;
-      }, 50);
+       x++;
+     }, 50);
 
 
 
     }
 
 
-    </script>
-    <script src="http://autobahn.s3.amazonaws.com/js/autobahn.min.js"></script>
+  </script>
+  <script src="http://autobahn.s3.amazonaws.com/js/autobahn.min.js"></script>
 
-    <script>
+  <script>
 
     //  loadWaitingForQuestionSingle('<?php echo $pusherIP; ?>' ,'<?php echo $_SESSION["game_id"]; ?>');
-    </script>
+  </script>
 
- </head>
- <body>
+</head>
+<body>
   <script src="../scripts/nouislider.min.js"></script>
 
-  <div id="overlayWrap">
-    			<a href="http://GameOn.World" id="logoLink"><img src="../img/logo.svg" id="logo"></a>
-      <form name="form1" method="post" action="submitAnswer.php">
-        <input name="questionNumber" type="hidden" value="<?php echo $_GET["question"] ?>">
-        <input type="hidden" id="answer" name="answer">
+
+   <a href="http://GameOn.World" id="logoLink"><img src="../img/logo.svg" id="logo"></a>
+   <form name="form1" method="post" action="submitAnswer.php">
+    <input name="questionNumber" type="hidden" value="<?php echo $_GET["question"] ?>">
+    <input type="hidden" id="answer" name="answer">
 
 
-          <center id="submitWrap">
-            <input type="text" id="isValue" name="isValue" value="5,000,000" readonly>
-            <div id="newSlider"></div>
-          </center>
-                      <input type="submit" name="submit" class="regButton" id="userMapSubmit" value="Submit!">
+    <center id="submitWrap">
+      <input type="text" id="isValue" name="isValue" value="5,000,000" readonly>
+      <div id="newSlider"></div>
+    </center>
+    <input type="submit" name="submit" class="regButton" id="userMapSubmit" value="Submit!">
 
           <!--
           <div id="relativeWrap">
@@ -166,40 +166,40 @@ if (isset($_GET["question"]))
               <option>70</option>
               <option>90</option>
             </datalist>
-            -->
+          -->
 
-          </div>
+        </div>
       </form>
-  </div>
+    </div>
 
     <div id="timerBar"></div>
-  <div id="questionWrap">
-    <div id="actualQuestion"><?php echo $theQuestion->getQuestionText(); ?> <?php echo $theQuestion->getLabel(); ?> <?php echo $theQuestion->getQuestionTextEnd(); ?>?</div>
-  </div>
+    <div id="questionWrap">
+      <div id="actualQuestion"><?php echo $theQuestion->getQuestionText(); ?> <?php echo $theQuestion->getLabel(); ?> <?php echo $theQuestion->getQuestionTextEnd(); ?>?</div>
+    </div>
 
 
     <script type="text/javascript">
 
 
 
-window.onload = function(){
+      window.onload = function(){
 
-      var timeleft = <?php echo $timeLeft; ?> * 1000;
-    console.log(timeleft);
-    $('#timerBar').animate({
-      width: "0%"
-    }, timeleft, "linear");
+        var timeleft = <?php echo $timeLeft; ?> * 1000;
+        console.log(timeleft);
+        $('#timerBar').animate({
+          width: "0%"
+        }, timeleft, "linear");
 
 
     var counter=setInterval(timer, <?php echo $timeLeft; ?> * 1000); //1000 will  run it every 1 second
 
     function timer()
     {
-         clearInterval(counter);
-       window.location.href = "showAnswerOther.php";
-       count=33333;
-         return;
-    }
+     clearInterval(counter);
+     window.location.href = "showAnswerOther.php";
+     count=33333;
+     return;
+   }
 
 
 
@@ -207,9 +207,9 @@ window.onload = function(){
 
 
 
-  var slider = document.getElementById('newSlider');
-  var valbox = document.getElementById("isValue");
-  var answer = document.getElementById("answer");
+   var slider = document.getElementById('newSlider');
+   var valbox = document.getElementById("isValue");
+   var answer = document.getElementById("answer");
 
   var realAnswer = <?php echo $region; ?>; ///  ************ change this to the correct answer
 
@@ -229,7 +229,7 @@ window.onload = function(){
   //   range = {   'min': [1600],    '25%': [1700],    '50%': [1800],    '75%': [1900],   'max': [2020] };
   // }
 
-    if (realAnswer == 0){
+  if (realAnswer == 0){
     range = {   'min': [-800],    '25%': [-600],    '50%': [-400],    '75%': [-200],   'max': [0] };
   }else if (realAnswer == 1){
     range = {   'min': [-200],    '25%': [0],    '50%': [200],    '75%': [400],   'max': [600] };
@@ -261,10 +261,10 @@ window.onload = function(){
     }else{
       a = Math.round(values[handle]);
     }
-      valbox.value = formatYear(a);
-      answer.value = a;
+    valbox.value = formatYear(a);
+    answer.value = a;
       //changeValue(values[handle]);
-  });
+    });
 
   labels = document.getElementsByClassName("noUi-value-large");
   for(var i=0; i<labels.length;i++){
@@ -286,34 +286,34 @@ window.onload = function(){
 };
 
 function formatYear(year){
-      if (year < 0){
-        return( (year * -1) + " BC" );
-      }else{
-        return(year);
-      }
+  if (year < 0){
+    return( (year * -1) + " BC" );
+  }else{
+    return(year);
+  }
 
 }
 
-  function decimalize(numberAsString){
-    var numberInt = parseInt(numberAsString)
-    var numberDecimalized = 2
-  }
+function decimalize(numberAsString){
+  var numberInt = parseInt(numberAsString)
+  var numberDecimalized = 2
+}
 
-  function comma(num){
-    num = num+"";
-    arr = num.split("");
-    newS = "";
-    for(var i=0; i<arr.length; i++){
-      if((arr.length - i)%i == 0 && i!=0)
-        newS = newS + "," + arr[i];
-      else
-        newS = newS + arr[i];
-    }
-    return newS;
+function comma(num){
+  num = num+"";
+  arr = num.split("");
+  newS = "";
+  for(var i=0; i<arr.length; i++){
+    if((arr.length - i)%i == 0 && i!=0)
+      newS = newS + "," + arr[i];
+    else
+      newS = newS + arr[i];
   }
+  return newS;
+}
 
 </script>
 
 
- </body>
- </html>
+</body>
+</html>
