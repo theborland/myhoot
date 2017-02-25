bda0d5fa604edd3e28f5a4ee046a7e6dd1b3e91f
borland2000@hotmail.com <borland2000@hotmail.com>
2017-02-24 19:58:15 -0500

    jeff

--- src/MyHoot/controller/User.php
+++ src/MyHoot/controller/User.php
@@ -120,7 +120,7 @@ class User{
 				}
 				$sql = "UPDATE `usersSingle` SET `tempAvg` = '".$tempAvg."',`score`='".$distanceAway."', `gamesPlayed` = '".$gamesPlayed."', `round` = '".$questionNumber."' WHERE user_id = '".$userID."'";
 				$result = $conn->query($sql);
-				die ($sql);
+				//die ($sql);
 				if (substr($last5Avg,0,1)!="~" && $last5Avg>$avg){
 					$gamesPlayed++;
 					//$newAvg=round(($avg*($gamesPlayed-1)+$place)/$gamesPlayed,2);
@@ -174,7 +174,7 @@ class User{
 				 {
 					 $sql = "SELECT * FROM `answers` WHERE game_id ='".$_SESSION["game_id"]."' AND questionNum='".$i."' AND user_id='".$userID."'";
 					 $result = $conn->query($sql);
-					 echo $sql;
+					 //echo $sql;
 					 if ($result && $row = $result->fetch_assoc())
 					    if ($row["avg"]!=0){
 								  echo "sss";
